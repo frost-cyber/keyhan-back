fde059c795ccc506ee4058ca15f03f886a886d83
Unknown <unknown@example.com>
2021-02-09 17:34:29 +0330

    CategoryRequest And StoreCategoryController
    Create add add to route

---
+++ app/Http/Controllers/StoreCategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class StoreCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+	
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param CategoryRequest $request
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+    public function store(CategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param CategoryRequest $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CategoryRequest $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
