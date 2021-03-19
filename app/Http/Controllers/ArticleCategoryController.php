73da09b3fb99ec15879b8d92b6acd81ac8be67e8
f-shokatabadi78 <75278683+f-shokatabadi78@users.noreply.github.com>
2021-03-19 09:44:00 +0330

    blog

---
+++ app/Http/Controllers/ArticleCategoryController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ArticleCategoryController extends Controller
+{ /**
+ * Display a listing of the resource.
+ * @return Response
+ */
+    public function index(Request $request)
+    {
+        $query = Category::query()->whereType(Category::TYPE_BLOG);
+
+        if (!$request->has('with_children')) {
+            $query = $query->without('children');
+        }
+
+        if ($request->has('children')){
+            $query = $query->where('parent_id' ,(int) $request->input('children'));
+        }
+
+        if ($request->has('parent')){
+            $query = $query->whereNull('parent_id');
+        }
+
+        return $query->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param CategoryRequest $request
+     * @return Response
+     */
+    public function store(CategoryRequest $request): Response
+    {
+        return \response([
+            'message'  => 'Create Category Successfully' ,
+            'category' => $this->save($request->all()) ,
+        ] , 200);
+    }
+
+    protected function save(array $data , Category $category = NULL): ?Category
+    {
+        //IF Create Category
+        if (!$category) {
+            $category = new Category();
+        }
+
+        $category->name = $data['name'];
+        $category->slug = \Str::slug($data['slug']);
+        $category->parent_id = $data['parent_id'] ?? NULL;
+        $category->type = Category::TYPE_BLOG;
+        $category->save();
+
+        return $category;
+    }
+
+    /**
+     * Display the specified resource.
+     * @param Category $category
+     * @return Category
+     */
+    public function show(Category $category): Category
+    {
+        return $category;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param CategoryRequest $request
+     * @param Category $category
+     * @return Response
+     */
+    public function update(CategoryRequest $request , Category $category): Response
+    {
+        return \response([
+            'message'  => 'Update Category Successfully' ,
+            'category' => $this->save($request->all() , $category) ,
+        ] , 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param Category $category
+     * @return Response
+     * @throws Exception
+     */
+    public function destroy(Category $category): Response
+    {
+        $category->delete();
+
+        return \response([
+            'message'  => 'Delete Category Successfully' ,
+            'category' => $category ,
+        ] , 200);
+    }
+}
---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles=Article::with(['file','category'])->get();
+        return $articles->toJson();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:5',
+            'body' => 'required',
+            'description' => 'required',
+            'slug' => 'required|unique:articles',
+            'tags' => 'required',
+            'category_id' => 'required|integer',
+            'image_id' => 'required|integer',
+            'status' => 'required|in:active,deactive'
+        ]);
+        $this->save($request->all(),new Article());
+        return response('create successfully','200');
+    }
+
+    public function show(Article $article){
+        return $article->load('')->append(['thumbnail']);
+    }
+
+    public function update(Request $request, Article $article)
+    {
+
+        $request->validate([
+            'title' => 'required|min:5',
+            'body' => 'required',
+            'description' => 'required',
+            'slug' => 'required|unique:articles',
+            'tags' => 'required',
+            'category_id' => 'required|integer',
+            'image_id' => 'required|integer',
+            'status' => 'required|in:active,deactive'
+        ]);
+        $this->save($request->all(), $article);
+        return response('update article successfully', 200);
+    }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return response('delete successfully', 200);
+    }
+
+    protected function save($data, Article $article)
+    {
+        $article->title = $data['title'];
+        $article->body = $data['body'];
+        $article->description = $data['description'];
+        $article->slug = $data['slug'];
+        $article->tags = $data['tags'];
+        $article->status = $data['status'];
+        $article->comments_count = $data['comments_count'] ?? $article->comments_count ?? 0;
+        $article->save();
+
+        $article->categories()->sync($data['category_id']);
+        $article->files()->sync($data['image_id'] , ['default' => true , 'description' => 'Thumbnail' , 'number' => 0]);
+
+        return $article;
+    }
+
+    public function tags()
+    {
+        $articlesTags = Article::all(['tags'])->pluck('tags')->toArray();
+        $tags=[];
+        foreach($articlesTags as $articleTags){
+            foreach ($articleTags as $tag){
+                if (!in_array($tag,$tags)){
+                    $tags[]=$tag;
+                }
+            }
+        }
+        return $tags;
+
+    }
+
+
+}
