cc5f2e92ce48b5c4ca4736f387bc033a23d4362c
Ali Talebi <frostcyber93@gmail.com>
2021-04-16 03:12:26 +0430

    sort api routes and add productcomment

--- app/Http/Controllers/ProductCommentController.php
+++ app/Http/Controllers/ProductCommentController.php
@@ -8,27 +8,66 @@ use Illuminate\Http\Request;
 
 class ProductCommentController extends Controller
 {
-    public function index(){
-        $comments = Comment::whereHasMorph('commentable' , Product::class );
+    public function index()
+    {
+        $comments = Comment::whereHasMorph('commentable' , Product::class);
 
-        if (\request()->has('confirmed')){
+        if (\request()->has('confirmed')) {
             $confirmed = (boolean)\request('confirmed');
         }
 
-        $comments->where('confirmed' , $confirmed??true);
+        $comments->where('confirmed' , $confirmed ?? TRUE);
         return $comments->latest()->get();
     }
 
-    public function show(Comment $comment){
-
+    public function show(Comment $comment)
+    {
+        return $comment->load([
+            'user' => function ($query) {
+                $query->select('name' , 'email');
+            },
+        ]);
     }
-    public function store(Request $request){
 
+    public function store(Request $request)
+    {
+        $comment = new Comment();
+
+        if (Auth()->check()) {
+            $comment->user_id = auth()->id();
+        } else {
+            $comment->name = $data['name'] ?? NULL;
+            $comment->email = $data['email'] ?? NULL;
+        }
+
+        if ($request->has('parent_id')) {
+            $comment->parent_id = (int)$request->parent_id ?: NULL;
+        }
+
+        $comment->body = $request->body;
+        Product::findOrFail($request->product_id)->comments()->save($comment);
+
+        return response('Create comment successfully');
     }
-    public function update(Request $request , Comment $comment){
 
+    public function update(Request $request , Comment $comment)
+    {
+        $comment->body = $request->body;
+        $comment->save();
+        return response('Update comment Successfully');
     }
-    public function destroy(Comment $comment){
 
+    public function toggleConfirm(Comment $comment)
+    {
+        $comment->confirmed = !$comment->confirmed;
+        $comment->save();
+        return $comment;
+    }
+
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+
+        return response('Delete comment successfully');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\ArticleCategoryController;
+use App\Http\Controllers\ArticleCommentController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\AttributeController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\BrandController;
+use App\Http\Controllers\FileController;
+use App\Http\Controllers\ProductCommentController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StoreCategoryController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,28 +24,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('auth/check.username' , [\App\Http\Controllers\Auth\LoginController::class , 'checkUsername']);
-Route::post('auth/login' , [\App\Http\Controllers\Auth\LoginController::class , 'login']);
-Route::post('auth/send.verify.code' , [\App\Http\Controllers\Auth\RegisterController::class , 'sendVerifyCode']);
-Route::post('auth/check.verify.code' , [\App\Http\Controllers\Auth\RegisterController::class , 'checkVerifyCode']);
-Route::post('auth/register' , [\App\Http\Controllers\Auth\RegisterController::class , 'register']);
+// Auth //
+Route::post('auth/check.username' , [LoginController::class , 'checkUsername']);
+Route::post('auth/login' , [LoginController::class , 'login']);
+Route::post('auth/send.verify.code' , [RegisterController::class , 'sendVerifyCode']);
+Route::post('auth/check.verify.code' , [RegisterController::class , 'checkVerifyCode']);
+Route::post('auth/register' , [RegisterController::class , 'register']);
 
-Route::apiResource('categories' , StoreCategoryController::class);
-Route::apiResource('articleCategories' , \App\Http\Controllers\ArticleCategoryController::class)->parameters(['articleCategories' => 'category']);
-Route::get('categoryArticle','\App\Http\Controllers\ArticleCategoryController@categoryArticle');
+// Store //
 Route::apiResource('attributes' , AttributeController::class);
-Route::apiResource('products' , ProductController::class)->whereNumber('product');
-Route::get('products/{slug}' , [ProductController::class , 'show'])->whereAlphaNumeric('product');
 Route::apiResource('brands' , BrandController::class);
-Route::get('articleComments/show_all',[\App\Http\Controllers\ArticleCommentController::class,'show_all']);
-Route::apiResource('articleComments' , \App\Http\Controllers\ArticleCommentController::class)->parameters(['articleComments'=>'comment'])->scoped(['comment'=>'id']);
-Route::put('articleComments/{comment}/toggleConfirm',[\App\Http\Controllers\ArticleCommentController::class,'toggleConfirm']);
-Route::get('tags',[\App\Http\Controllers\ArticleController::class,'tags']);
-Route::put('articleComments/{comment}/toggleConfirm',[\App\Http\Controllers\ArticleCommentController::class,'toggleConfirm']);
-Route::apiResource('articleComments' , \App\Http\Controllers\ArticleCommentController::class)->scoped(['comment'=>'id']);
-Route::apiResource('articles',\App\Http\Controllers\ArticleController::class)->scoped(['article' =>'slug']);
-Route::apiResource('product/comments' , \App\Http\Controllers\ProductCommentController::class)->parameters(['product/comments' => 'comment']);
+Route::apiResource('storeCategories' , StoreCategoryController::class)->parameters(['storeCategories'=> 'category']);
+Route::apiResource('products' , ProductController::class)->whereNumber('product');
+Route::get('products/{slug}' , [ProductController::class , 'show']);
+Route::apiResource('product/comments' , ProductCommentController::class)->parameters(['product/comments' => 'comment']);
+Route::put('product/comments/{comment}/toggleConfirm',[ProductCommentController::class , 'toggleConfirm']);
 
+//-- Blog --//
+Route::apiResource('articles', ArticleController::class)->scoped(['article' =>'slug']);
+Route::get('tags',[ArticleController::class , 'tags']);
+Route::apiResource('articleCategories' , ArticleCategoryController::class)->parameters(['articleCategories' => 'category']);
+Route::get('categoryArticle','\App\Http\Controllers\ArticleCategoryController@categoryArticle');
+Route::apiResource('articleComments' , ArticleCommentController::class)->parameters(['articleComments'=>'comment'])->scoped(['comment'=>'id']);
+Route::apiResource('articleComments' , ArticleCommentController::class)->scoped(['comment'=>'id']);
+Route::put('articleComments/{comment}/toggleConfirm',[ArticleCommentController::class , 'toggleConfirm']);
+
+
+// App //
 Route::group(['prefix' => 'files'] , function(){
-    Route::post('upload' , [\App\Http\Controllers\FileController::class , 'upload']);
+    Route::post('upload' , [FileController::class , 'upload']);
 });
