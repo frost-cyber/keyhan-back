d0b0b6bde300fde07d793317b15541406885acb1
ftm.sh <f.shokatabadi78@gmail.com>
2021-06-13 15:38:05 +0430

    set role

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,84 +7,88 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable {
-    use HasFactory, Notifiable, HasApiTokens, MustVerifyEmail;
-    const RELATIONS = [
-        'productsWishlist',
-        'carts',
-    ];
+	use HasFactory, Notifiable, HasApiTokens, MustVerifyEmail, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'last_name',
-        'phone',
-        'national_code',
-        'email',
-        'password',
-        'mobile',
-        'avatar',
-        'is_admin',
-    ];
+	const RELATIONS = [
+		'productsWishlist',
+		'carts',
+	];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name',
+		'last_name',
+		'phone',
+		'national_code',
+		'email',
+		'password',
+		'mobile',
+		'avatar',
+		'is_admin',
+	];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at'  => 'datetime',
-        'mobile_verified_at' => 'datetime',
-    ];
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
 
-    public static function ALL_RELATIONS() {
-        $relations = static::RELATIONS;
+	/**
+	 * The attributes that should be cast to native types.
+	 *
+	 * @var array
+	 */
+	protected $casts = [
+		'email_verified_at'  => 'datetime',
+		'mobile_verified_at' => 'datetime',
+	];
 
-        foreach ( Product::ALL_RELATIONS() as $item ) {
-            $relations[] = 'productsWishlist.' . $item;
-        }
+	public static function ALL_RELATIONS() {
+		$relations = static::RELATIONS;
 
-        foreach ( Cart::ALL_RELATIONS() as $item ) {
-            $relations[] = 'carts.' . $item;
-        }
+		foreach ( Product::ALL_RELATIONS() as $item ) {
+			$relations[] = 'productsWishlist.' . $item;
+		}
 
-        return $relations;
-    }
+		foreach ( Cart::ALL_RELATIONS() as $item ) {
+			$relations[] = 'carts.' . $item;
+		}
 
-    public function productsWishlist() {
-        return $this->morphedByMany( Product::class, 'wishable', 'wishlist' );
-    }
+		return $relations;
+	}
 
-    public function carts(){
-        return $this->hasMany(Cart::class);
-    }
+	public function productsWishlist() {
+		return $this->morphedByMany( Product::class, 'wishable', 'wishlist' );
+	}
 
-    public function currentCart(): Cart{
-        return $this->carts()->where('status', 0)->firstOrCreate();
-    }
+	public function carts() {
+		return $this->hasMany( Cart::class );
+	}
 
-    public function files(){
-        return $this->morphToMany(File::Class , 'fileable' , 'fileables');
-    }
-    public function addresses(){
-        return $this->hasMany(Address::class);
-    }
-    public function getAvatarAttribute(){
-        return $this->files[0] ?? [] ;
-    }
+	public function currentCart(): Cart {
+		return $this->carts()->where( 'status', 0 )->firstOrCreate();
+	}
+
+	public function files() {
+		return $this->morphToMany( File::Class, 'fileable', 'fileables' );
+	}
+
+	public function addresses() {
+		return $this->hasMany( Address::class );
+	}
+
+	public function getAvatarAttribute() {
+		return $this->files[0] ?? [];
+	}
 }
