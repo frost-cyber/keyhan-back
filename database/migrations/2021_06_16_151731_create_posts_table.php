e3ecd14d307900665071feef06955bbae5e0ad3d
ftm.sh <f.shokatabadi78@gmail.com>
2021-06-16 15:52:19 +0430

    post

---
+++ database/migrations/2021_06_16_151731_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->json('cities');
+            $table->json('states');
+            $table->integer('start_price')->unsigned()->nullable();
+            $table->integer('price_per_kilo')->unsigned()->nullable();
+            $table->boolean('is_free')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
