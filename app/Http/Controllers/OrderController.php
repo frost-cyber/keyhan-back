656823dad36b1db981d72efda05dce046c6dbb3a
f-shokatabadi78 <75278683+f-shokatabadi78@users.noreply.github.com>
2021-06-27 20:07:40 +0430

    change order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,74 +3,96 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Shipment;
 use App\Models\Cart;
+use Shetabit\Multipay\Invoice;
+use Shetabit\Multipay\RedirectionForm;
+use Shetabit\Payment\Facade\Payment;
 
 class OrderController extends Controller {
-    public function payCart() {
-        $cart = auth()->user()->currentCart();
+	public function payCart() {
+		$cart = auth()->user()->currentCart();
+		$this->validateCart( $cart );
+		$order = auth()->user()->orders()->create( [
+			'order_number' => random_int( 100000, 999999 ),
+		] );
 
-        $this->validateCart($cart);
+		$this->orderAttachVariants( $order, $cart );
+		$shipment = new Shipment( [
+			'status'     => 'درحال ارسال',
+			'address_id' => 1,
+		] );
+		$shipment->address()->associate( 1 );
+		$order->shipments()->save( $shipment );
+		$pay = $this->pay( $order->total_price );
+		$order->payments()->create( [
+			'gateway' => 'زرین پال',
+			'status'  => 'پرداخت نشده',
+			'data'    => [ 'authority' => $pay['transactionId'] ],
+		] );
+		$cart->delete();
 
-        $order = auth()->user()->orders()->create( [
-            'order_number' => random_int( 100000, 999999 ),
-        ] );
+		return $pay['redirectForm']->toJson();
+	}
 
-        $orderProductVariants = [];
-        foreach ( $cart->productVariants as $variant ) {
+	private function orderAttachVariants( Order $order, Cart $cart ) {
+		$orderProductVariants = [];
+		$orderTotalPrice      = 0;
+		$cart->productVariants->each( function ( $variant ) use ( &$orderProductVariants, &$orderTotalPrice ) {
 
-            $priceType = 'selling_price';
-            if ( $variant->pivot->quantity >= $variant->minimum_wholesale ) {
-                $priceType = 'wholesale_price';
-            } elseif ( $variant->discounted_price ) {
-                $priceType = 'discounted_price';
-            }
+			$priceType = 'selling_price';
+			if ( $variant->pivot->quantity >= $variant->minimum_wholesale ) {
+				$priceType = 'wholesale_price';
+			} elseif ( $variant->discounted_price ) {
+				$priceType = 'discounted_price';
+			}
+			$orderProductVariants[ $variant->id ] = [
+				'purchase_price' => $variant->purchase_price,
+				'price'          => $variant->{$priceType},
+				'price_type'     => $priceType,
+				'quantity'       => $variant->pivot->quantity,
+				'total_price'    => $variant->pivot->quantity * $variant->{$priceType},
+				'extra'          => json_encode( [ 'variant' => $variant->getAttributes() ] ),
+			];
 
-            $orderProductVariant[ $variant->id ] = [
-                'purchase_price' => $variant->purchase_price,
-                'price' => $variant->{$priceType},
-                'price_type' => $priceType,
-                'quality' => $variant->pivot->quantity,
-            ];
-        }
+			$orderTotalPrice += $variant->pivot->quantity * $variant->{$priceType};
+		} );
+		$order->total_price = $orderTotalPrice;
+		$order->save();
+		$order->productVariants()->sync( $orderProductVariants );
+	}
 
-        $order->productVariants()->attach( $orderProductVariants );
+	private function validateCart( Cart $cart ) {
+		if ( $cart->productVariants->isEmpty() ) {
+			abort( 400, 'سبد خرید خالی است' );
+		}
+		$productVariantsNotEnoughInventory = [];
+		foreach ( $cart->productVariants as $productVariant ) {
+			if ( $productVariant->inventory < $productVariant->pivot['quantity'] ) {
+				$productVariantsNotEnoughInventory[] = $productVariant;
+			}
+		}
+		if ( $productVariantsNotEnoughInventory ) {
+			$msg = 'محصولات زیر موجودی آن کافی نیست';
+			foreach ( $productVariantsNotEnoughInventory as $productVariant ) {
+				$msg .= " </br>" . $productVariant->product->name;
+			}
 
-        $shipment = $order->shipments()->newModelInstance( [
-            'status' => 'درحال ارسال',
-        ] );
+			abort( 400, $msg );
+		}
+		if ( ! $cart->address ) {
+			abort( 400, 'ادرسی وجود ندارد' );
+		}
+	}
 
-        $shipment->address()->associate( 1 );
+	private function pay( $amount ) {
+		$invoice = ( new Invoice() )->amount( $amount );
 
-        $order->payments()->create([
-            'gateway' => '',
-            'status' => 'پرداخت نشده'
-        ]);
-
-        return $shipment->save();
-    }
-
-    private function validateCart(Cart $cart){
-        if ( $cart->productVariants->isEmpty() ) {
-            return response( 'سبد خرید خالی است', 400 );
-        }
-        $productVariantsNotEnoughInventory = [];
-        foreach ( $cart->productVariants as $productVariant ) {
-            if ( $productVariant->inventory < $productVariant->pivot['quantity'] ) {
-                $productVariantsNotEnoughInventory[] = $productVariant;
-            }
-        }
-        if ( $productVariantsNotEnoughInventory ) {
-            $msg = 'محصولات زیر موجودی آن کافی نیست';
-            foreach ( $productVariantsNotEnoughInventory as $productVariant ) {
-                $msg .= " </br>" . $productVariant->product->name;
-            }
-
-            return response( $msg, 400 );
-        }
-        if ( ! $cart->address ) {
-            return response( 'ادرسی وجود ندارد', 400 );
-        }
-    }
+		return [
+			'redirectForm'  => Payment::purchase( $invoice )->pay(),
+			'transactionId' => $invoice->getTransactionId(),
+		];
+	}
 }
